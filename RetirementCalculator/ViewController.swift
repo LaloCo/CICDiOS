a0856881ca6a9c432c7569311fbf15f7e1575598
Eduardo Rosas <lalorosas@outlook.com>
2019-08-19 07:20:17 -0500

    Initial Commit

---
+++ RetirementCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RetirementCalculator
+//
+//  Created by Eduardo Rosas on 8/19/19.
+//  Copyright © 2019 Eduardo Rosas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
