1eb8665a1d6d99b809434738227ed670a04afd20
Eduardo Rosas <lalorosas@outlook.com>
2019-08-20 11:22:55 -0500

    Crash the application on button click

--- RetirementCalculator/ViewController.swift
+++ RetirementCalculator/ViewController.swift
@@ -7,14 +7,25 @@
 //
 
 import UIKit
+import AppCenterCrashes
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var monthlyInvestmentsTextField: UITextField!
+    @IBOutlet weak var ageTextField: UITextField!
+    @IBOutlet weak var retirementAgeTextField: UITextField!
+    @IBOutlet weak var interestRateTextField: UITextField!
+    @IBOutlet weak var savingsTextField: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func calculateButton_TouchUpInside(_ sender: Any) {
+        MSCrashes.generateTestCrash()
+    }
+    
 }
 
