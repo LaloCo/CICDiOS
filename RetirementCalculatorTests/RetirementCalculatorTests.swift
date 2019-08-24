37ed55fe99d934892c512a4ac020d7098299e918
Eduardo Rosas <lalorosas@outlook.com>
2019-08-24 12:01:05 -0500

    Test unit test

--- RetirementCalculatorTests/RetirementCalculatorTests.swift
+++ RetirementCalculatorTests/RetirementCalculatorTests.swift
@@ -22,6 +22,7 @@ class RetirementCalculatorTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssertEqual("", " ")
     }
 
     func testPerformanceExample() {
